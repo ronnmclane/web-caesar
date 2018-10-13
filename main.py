1975a36b5541d77db3750fe83b1c73899385ab0d
Ronn McLane <ronnmclane@gmail.com>
2018-10-13 12:54:14 -0500

    Final Finshed web caesar

--- main.py
+++ main.py
@@ -8,36 +8,36 @@ app.config['DEBUG'] = True
 
 form = """
 
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <style>
-                form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            form {{
+            background-color: #eee;
+            padding: 20px;
+            margin: 0 auto;
+            width: 540px;
+            font: 16px sans-serif;
+            border-radius: 10px;
+        }}
 
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
+        textarea {{
+            margin: 10px 0;
+            width: 540px;
+            height: 120px;
+        }}
 
-            </style>
-        </head>
-        <body>
-            <form action="/" method="post">
-                <label>Rotate by: <input type="text" name="rot" value="0"></label><br>
-                <textarea name="text" cols="70" rows="10">{0}</textarea><br>
-                <input type="submit" value="Submit Query" name="submit">
-            </form>
-        </body>
-    </html>
-    """
+        </style>
+    </head>
+    <body>
+        <form action="/" method="post">
+            <label>Rotate by: <input type="text" name="rot" value="0"></label><br>
+            <textarea name="text" cols="70" rows="10">{0}</textarea><br>
+            <input type="submit" value="Submit Query" name="submit">
+        </form>
+    </body>
+</html>
+"""
 
 @app.route("/")
 
